c01bdada577d84bf1d734d30007325af539197df
BusinessEmpireHTML <businessempire767@gmail.com>
2025-12-25 22:02:39 -0600

    Update bible-proxy.js

--- netlify/functions/bible-proxy.js
+++ netlify/functions/bible-proxy.js
@@ -37,7 +37,7 @@ exports.handler = async (event, context) => {
             // Bolls Life uses specific translation codes:
       // WLC = Westminster Leningrad Codex (Hebrew)
       // TR  = Textus Receptus (Greek) 
-      const languageCode = testament === 'ot' ? 'WLC' : 'TISCH';
+      const languageCode = testament === 'ot' ? 'WLC' : 'LXX';
       const url = `https://bolls.life/get-chapter/${languageCode}/${book}/${chapter}/`;
       
       console.log('Fetching original from:', url);
